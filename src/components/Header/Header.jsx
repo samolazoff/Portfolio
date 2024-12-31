cc2a79e385fa3fa129a7b4b38752121b38e715b4
Samolazoff <samolazov.roman.alex@gmail.com>
2024-12-31 16:29:03 +0300

    Add dirty app-header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import './Header.scss';
+
+function Header() {
+    return (
+        <header className="app-header">
+            <div className="container">
+                <h1 className="logo">Samolazoff</h1>
+                <nav className="app-header-nav">
+                    <ul>
+                        <li>
+                            <a href=""></a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
--- src/pages/Layout/Layout.jsx
+++ src/pages/Layout/Layout.jsx
@@ -2,10 +2,12 @@ import './Layout.scss';
 
 import { Outlet } from 'react-router';
 
+import Header from '../../components/Header/Header';
+
 function Layout () {
     return(
         <>
-            <header className="app-header"></header>
+            <Header/>
             <main className='app-main'>
                 <Outlet/>
             </main>
