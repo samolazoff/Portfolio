f7a9711ba5cdb87267cdefb0952beb6935bd7a21
Samolazoff <samolazov.roman.alex@gmail.com>
2024-12-31 17:00:29 +0300

    Add Logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,12 @@
 import './Header.scss';
 
+import Logo from '../Logo/Logo';
+
 function Header() {
     return (
         <header className="app-header">
             <div className="container">
-                <h1 className="logo">Samolazoff</h1>
+                <Logo/>
                 <nav className="app-header-nav">
                     <ul>
                         <li>
