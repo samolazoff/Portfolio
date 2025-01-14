2ea4b23271f18b88e665209d8b576c55958ce370
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-14 12:41:39 +0300

    Create page Blog

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {BrowserRouter, Routes, Route} from 'react-router';
 
 import Layout from './pages/Layout/Layout';
 import Home from './pages/Home/Home';
+import Blog from './pages/Blog/Blog';
 
 function App() {
 return (
@@ -11,6 +12,7 @@ return (
         <Routes>
             <Route path='/' element={<Layout></Layout>}>
                 <Route index element={<Home></Home>}/>
+                <Route path='/blog' element={<Blog></Blog>}/>
             </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/pages/Blog/Blog.jsx
@@ -0,0 +1,11 @@
+import './Blog.scss';
+
+function Blog () {
+    return(
+        <section className="page-blog container container_page">
+                
+        </section>
+    )
+};
+
+export default Blog;
