c8d7d84d041464f48bb8cd5fdb0b3cc6140960c2
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-15 11:47:32 +0300

    Create conseption of Contacts

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,9 +1,27 @@
 import './Contacts.scss';
 
+import {Link} from 'react-router';
+
+import FormForConection from '../../components/FormForConection/FormForConection';
+
 const Contacts = () => {
     return (
         <section className='page-contacts container container_page'>
-            
+            <FormForConection></FormForConection>
+            <div className="page-contacts-box">
+                <div className="contact-box">
+                    <img src="" alt="" className="contact-box__img" />
+                    <address>
+                        <Link to='mailto:samolazov.roman.alex@gmail.com'>samolazov.roman.alex@gmail.com</Link>
+                    </address>
+                </div>
+                <div className="contact-box">
+                    <img src="" alt="" className="contact-box__img" />
+                    <address>
+                        <Link to='tel:+375445576259'>+375445576259</Link>
+                    </address>
+                </div>
+            </div>
         </section>
     );
 };
