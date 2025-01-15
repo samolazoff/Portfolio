4ec086da30a04a68aa19b6ba84ebb62045d668ec
Samolazoff <samolazov.roman.alex@gmail.com>
2025-01-15 11:34:13 +0300

    Add contacts block

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Layout from './pages/Layout/Layout';
 import Home from './pages/Home/Home';
 import Blog from './pages/Blog/Blog';
 import Portfolio from './pages/Portfolio/Portfolio';
+import Contacts from './pages/Contacts/Contacts';
 
 function App() {
 return (
@@ -15,6 +16,7 @@ return (
                 <Route index element={<Home></Home>}/>
                 <Route path='/blog' element={<Blog></Blog>}/>
                 <Route path='/portfolio' element={<Portfolio></Portfolio>}/>
+                <Route path='/contacts' element={<Contacts></Contacts>}/>
             </Route>
         </Routes>
     </BrowserRouter>
---
+++ src/pages/Contacts/Contacts.jsx
@@ -0,0 +1,11 @@
+import './Contacts.scss';
+
+const Contacts = () => {
+    return (
+        <section className='page-contacts container container_page'>
+            
+        </section>
+    );
+};
+
+export default Contacts;
